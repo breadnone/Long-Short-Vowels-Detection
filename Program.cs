cf37fc9dcf0f4fd7599171364769f1d972b0d5f9
Cattywampus <64100867+breadnone@users.noreply.github.com>
2022-03-13 13:31:56 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,4 +9,24 @@ public class Program
 	{
 		Console.WriteLine("Hello World");
 	}
+
+    public double LongVowel()
+    {
+        var t = 0.0;
+
+        return t;
+    }
+    public double ShortVowel()
+    {
+        var t = 0.0;
+
+        return t;
+    }
+
+    public double LongI()
+    {
+        var t = 0.0;
+
+        return t;
+    }
 }
